38806606ddadeae2e1788e539636aaaac45511de
Raven <redstoyningbros@gmail.com>
2020-11-03 02:40:17 +0200

    Added credentials for db connection

--- src/main/java/ua/lviv/iot/terminal_jdbc/App.java
+++ src/main/java/ua/lviv/iot/terminal_jdbc/App.java
@@ -1,5 +1,13 @@
 package ua.lviv.iot.terminal_jdbc;
 
+import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.List;
+
+import ua.lviv.iot.terminal_jdbc.model.dataaccess.CountryDataAccess;
+import ua.lviv.iot.terminal_jdbc.model.dataaccess.implementation.CountryDataAceessImpl;
+import ua.lviv.iot.terminal_jdbc.model.entity.CountryEntity;
+
 /**
  * Hello world!
  *
@@ -8,6 +16,13 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+      CountryDataAccess data = new CountryDataAceessImpl();
+      List<CountryEntity> counties;
+      try {
+        counties = data.findAll();
+        System.out.println(counties);
+      } catch (SQLException e) {
+        e.printStackTrace();
+      }
     }
 }
